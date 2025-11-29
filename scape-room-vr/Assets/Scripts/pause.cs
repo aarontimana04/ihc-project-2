989f368e5701bd17a07d413861af976e0eb7bee8
LABORATORIO L507 <LABORATORIO-L507@L507-011.utec.local>
2025-11-29 12:19:50 -0500

    fix: bugs

---
+++ scape-room-vr/Assets/Scripts/pause.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuController4 : MonoBehaviour
+{
+    public GameObject menu;
+    public CountdownClock countdown;
+
+    private void Start()
+    {
+        menu.SetActive(false);
+        enabled = false; 
+    }
+
+    public void ToggleMenu(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+
+        bool show = !menu.activeSelf;
+        menu.SetActive(show);
+
+        if (countdown != null)
+        {
+            if (show)
+                countdown.StopCountdown();
+            else
+                countdown.StartCountdown();
+        }
+    }
+
+    // Llamar desde el botón Play
+    public void EnablePauseSystem()
+    {
+        enabled = true; // Activar el script
+    }
+}
